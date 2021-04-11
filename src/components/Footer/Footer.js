519fc3446d4435f8c5cd519dbd520bf26e113fae
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-10 19:41:20 -0600

    update footer sections

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -24,19 +24,19 @@ const Footer = () => {
           <SocialMediaWrap>
             <SocialLogo to="/">Copyright &copy; Moses Kaumatule</SocialLogo>
             <SocialIcons>
-              <SocialIconLink href="/" target="_blank" aria-label="LinkedIn"
+              <SocialIconLink href="https://www.linkedin.com/in/mosesvk/" target="_blank" aria-label="LinkedIn"
               rel="noopener noreferrer">
                 <FaLinkedin />
               </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Github"
+              <SocialIconLink href="https://github.com/mosesvk" target="_blank" aria-label="Github"
               rel="noopener noreferrer">
                 <FaGithubSquare />
               </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Facebook"
+              <SocialIconLink href="https://www.facebook.com/profile.php?id=100004199235959" target="_blank" aria-label="Facebook"
               rel="noopener noreferrer">
                 <FaFacebookSquare />
               </SocialIconLink>
-              <SocialIconLink href="/" target="_blank" aria-label="Youtube"
+              <SocialIconLink href="https://www.youtube.com/channel/UChlB6LMekxCh917bjumDcCA" target="_blank" aria-label="Youtube"
               rel="noopener noreferrer">
                 <FaYoutubeSquare />
               </SocialIconLink>
