8ee31ffdc653897a4736cc3333587358e9ff9d60
Moses Kaumatule <kaumatule93@gmail.com>
2021-04-23 01:57:47 -0600

    update

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,18 +1,7 @@
 import React, {useRef} from "react";
-// import {Button} from '../ButtonElements'
 import {
-  FaLinkedin,
-  FaGithub,
   FaTimes
 } from 'react-icons/fa'
-import {
-  PortContainer,
-  PortHeader,
-  PortText,
-  PortP,
-  SocialWrap,
-  SocialLink,
-} from "./portfolioElements";
 import Divider from '@material-ui/core/Divider';
 import Modal from '../Modal/Modal'
 import ModalTwo from '../Modal/ModalTwo'
@@ -37,54 +26,69 @@ const Portfolio = () => {
   }
 
   return (
-    <div className='program-container'>
-      <div className='program-wrap'>
-        <div className="card">
-          <div className="card__image">
-            <div className="blue-filter card-1-img">
-              <button className="btn-reveal" onClick={openModal}>See Details</button>
-            </div>
-          </div>
-          <div className="price-title">
-            <h2 className="ca rd-title card-title-1">POWERHOUSE FITNESS</h2>
-          </div>
-        </div>
-        <div className="card">
-          <div className="card__image">
-            <div className="blue-filter card-2-img">
-              <button className="btn-reveal" onClick={openModal2}>See Details</button>
-            </div>
-          </div>
-          <div className="price-title">
-            <h2 className="card-title card-title-2">BARZ</h2>
-          </div>
-
-        </div>
-        <Modal ref={modalRef}>
-          <button onClick={() => modalRef.current.close()}><FaTimes/></button>
-          <h1>Individual Training</h1>
-          <p>
-            loremNostrud ipsum dolor culpa dolore nulla sunt ullamco laboris ad commodo deserunt. Amet magna sit cillum adipisicing voluptate ullamco duis cupidatat aliqua qui occaecat Lorem et ea. Ipsum amet ullamco voluptate cillum anim in velit quis ea aliquip fugiat nisi exercitation reprehenderit.
-          </p>
-        </Modal>
-        <ModalTwo ref={modalRef2}>
-          <button onClick={() => modalRef2.current.close()}><FaTimes/></button>
-          <h1 className="modal-h1">Group Training</h1>
-          <p className="modal-p">
-            loremNostrud ipsum dolor culpa dolore nulla sunt ullamco laboris ad commodo deserunt. Amet magna sit cillum adipisicing voluptate ullamco duis cupidatat aliqua qui occaecat Lorem et ea. Ipsum amet ullamco voluptate cillum anim in velit quis ea aliquip fugiat nisi exercitation reprehenderit.
-          </p>
-        </ModalTwo>
-        <ModalThree ref={modalRef3}>
-          <button onClick={() => modalRef3.current.close()}><FaTimes/></button>
-          <h1 className="modal-h1">Group Training</h1>
-          <p className="modal-p">
-            loremNostrud ipsum dolor culpa dolore nulla sunt ullamco laboris ad commodo deserunt. Amet magna sit cillum adipisicing voluptate ullamco duis cupidatat aliqua qui occaecat Lorem et ea. Ipsum amet ullamco voluptate cillum anim in velit quis ea aliquip fugiat nisi exercitation reprehenderit.
-          </p>
-        </ModalThree>
-      </div>
+    <div className='program-container' 
+    id="portfolio">
+    <div className='program-header'>
+      <h1>PORTFOLIO</h1>
+      <Divider/>
     </div>
+    <div className='program-wrap'>
+      <div className="card">
+        <div className="card__image">
+          <div className="blue-filter card-1-img">
+            <button className="btn-reveal" onClick={openModal}>See Details</button>
+          </div>
+        </div>
+        <div className="price-title">
+          <h2 className="ca rd-title card-title-1">POWERHOUSE FITNESS</h2>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image">
+          <div className="blue-filter card-2-img">
+            <button className="btn-reveal" onClick={openModal2}>See Details</button>
+          </div>
+        </div>
+        <div className="price-title">
+          <h2 className="card-title card-title-2">CHEEFA'S FRIED PIES</h2>
+        </div>
+      </div>
+      <div className="card">
+        <div className="card__image">
+          <div className="blue-filter card-2-img">
+            <button className="btn-reveal" onClick={openModal3}>See Details</button>
+          </div>
+        </div>
+        <div className="price-title">
+          <h2 className="card-title card-title-2">BARZ</h2>
+        </div>
+      </div>
+      <Modal ref={modalRef}>
+        <button onClick={() => modalRef.current.close()}><FaTimes/></button>
+        <h1>Individual Training</h1>
+        <p>
+          loremNostrud ipsum dolor culpa dolore nulla sunt ullamco laboris ad commodo deserunt. Amet magna sit cillum adipisicing voluptate ullamco duis cupidatat aliqua qui occaecat Lorem et ea. Ipsum amet ullamco voluptate cillum anim in velit quis ea aliquip fugiat nisi exercitation reprehenderit.
+        </p>
+      </Modal>
+      <ModalTwo ref={modalRef2}>
+        <button onClick={() => modalRef2.current.close()}><FaTimes/></button>
+        <h1 className="modal-h1">Group Training</h1>
+        <p className="modal-p">
+          loremNostrud ipsum dolor culpa dolore nulla sunt ullamco laboris ad commodo deserunt. Amet magna sit cillum adipisicing voluptate ullamco duis cupidatat aliqua qui occaecat Lorem et ea. Ipsum amet ullamco voluptate cillum anim in velit quis ea aliquip fugiat nisi exercitation reprehenderit.
+        </p>
+      </ModalTwo>
+      <ModalThree ref={modalRef3}>
+        <button onClick={() => modalRef3.current.close()}><FaTimes/></button>
+        <h1 className="modal-h1">Group Training</h1>
+        <p className="modal-p">
+          loremNostrud ipsum dolor culpa dolore nulla sunt ullamco laboris ad commodo deserunt. Amet magna sit cillum adipisicing voluptate ullamco duis cupidatat aliqua qui occaecat Lorem et ea. Ipsum amet ullamco voluptate cillum anim in velit quis ea aliquip fugiat nisi exercitation reprehenderit.
+        </p>
+      </ModalThree>
+    </div>
+  </div>
   );
 };
 
 export default Portfolio;
+
 
