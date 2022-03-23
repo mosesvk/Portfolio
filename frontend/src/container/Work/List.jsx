85ba4a6f08bf310d53e4fe47ab8613b9735467c0
mosesvk <kaumatule93@gmail.com>
2022-03-23 14:09:02 -0600

    customizing works section

--- frontend/src/container/Work/List.jsx
+++ frontend/src/container/Work/List.jsx
@@ -2,37 +2,44 @@ import React, { useState, useEffect } from 'react';
 import { items } from './data';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
+
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
 import { urlFor, client } from '../../client';
 
-
-function Card({ id, title, category, theme }) {
-
+function CardItem({ id, title, category, theme }) {
   return (
-    <li className={`card ${theme}`}>
-      <div className='card-content-container'>
-        <motion.div whileHover={{ opacity: [0, 1] }} className='card-content' layoutId={`card-container-${id}`}>
-          <motion.div
-            className='card-image-container'
-            layoutId={`card-image-container-${id}`}
-          >
-            <img className='card-image' src={`images/${id}.jpg`} alt='' />
-          </motion.div>
-          <motion.div
-            className='title-container'
-            layoutId={`title-container-${id}`}
-          >
-            <span className='category'>{category}</span>
-            <h2>{title}</h2>
-          </motion.div>
-        </motion.div>
-      </div>
-      <Link to={id} className={`card-open-link`} />
-    </li>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component='img'
+          height='140'
+          src={`images/${id}.jpg`}
+          alt={title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div'>
+            {title}
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            {category}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size='small' color='primary'>
+          Share
+        </Button>
+      </CardActions>
+    </Card>
   );
 }
 
 export function List({ selectedId }) {
-
   // const [items, setItems] = useState([])
 
   // useEffect(() => {
@@ -42,12 +49,34 @@ export function List({ selectedId }) {
   //   });
   // }, []);
 
-
   return (
     <ul className='card-list'>
       {items.map((card) => (
-        <Card key={card.id} {...card} isSelected={card.id === selectedId} />
+        <CardItem key={card.id} {...card} isSelected={card.id === selectedId} />
       ))}
     </ul>
   );
+}
+
+{
+  /* <li className={`card ${theme}`}>
+<div className='card-content-container'>
+  <motion.div whileHover={{ opacity: [0, 1] }} className='card-content' layoutId={`card-container-${id}`}>
+    <motion.div
+      className='card-image-container'
+      layoutId={`card-image-container-${id}`}
+    >
+      <img className='card-image' src={`images/${id}.jpg`} alt='' />
+    </motion.div>
+    <motion.div
+      className='title-container'
+      layoutId={`title-container-${id}`}
+    >
+      <span className='category'>{category}</span>
+      <h2>{title}</h2>
+    </motion.div>
+  </motion.div>
+</div>
+<Link to={id} className={`card-open-link`} />
+</li> */
 }
