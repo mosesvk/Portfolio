59f8e0eb56a0aadd16e785c6949883e9988708d6
mosesvk <kaumatule93@gmail.com>
2022-03-22 22:58:08 -0600

    customizing works section

--- frontend/src/container/Work/Item.jsx
+++ frontend/src/container/Work/Item.jsx
@@ -1,42 +1,32 @@
-import React from "react";
-import { motion } from "framer-motion";
-import { LoremIpsum } from "react-lorem-ipsum";
-import { Link } from "react-router-dom";
-import { items } from "./data";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { LoremIpsum } from 'react-lorem-ipsum';
+import { Link } from 'react-router-dom';
+import { items } from './data';
 
 export function Item({ id }) {
-  const newItems = items.find(item => item.id === id);
+  const newItems = items.find((item) => item.id === id);
   // console.log(newItems)
-  const { category, title } = newItems
+  const { category, title } = newItems;
 
   return (
     <>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0, transition: { duration: 0.15 } }}
-        transition={{ duration: 0.2, delay: 0.15 }}
-        style={{ pointerEvents: "auto" }}
-        className="overlay"
-      >
-        <Link to="/" />
-      </motion.div>
-      <div className="card-content-container open">
-        <motion.div className="card-content" layoutId={`card-container-${id}`}>
+      <div className='card-content-container open'>
+        <motion.div className='card-content' layoutId={`card-container-${id}`}>
           <motion.div
-            className="card-image-container"
+            className='card-image-container'
             layoutId={`card-image-container-${id}`}
           >
-            <img className="card-image" src={`images/${id}.jpg`} alt="" />
+            <img className='card-image' src={`images/${id}.jpg`} alt='' />
           </motion.div>
           <motion.div
-            className="title-container"
+            className='title-container'
             layoutId={`title-container-${id}`}
           >
-            <span className="category">{category}</span>
+            <span className='category'>{category}</span>
             <h2>{title}</h2>
           </motion.div>
-          <motion.div className="content-container" animate>
+          <motion.div className='content-container' animate>
             <LoremIpsum
               p={1}
               avgWordsPerSentence={3}
@@ -45,6 +35,16 @@ export function Item({ id }) {
           </motion.div>
         </motion.div>
       </div>
+      <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0, transition: { duration: 0.15 } }}
+          transition={{ duration: 0.2, delay: 0.15 }}
+          style={{ pointerEvents: 'auto' }}
+          className='overlay'
+        >
+          <Link to='/' />
+        </motion.div>
     </>
   );
 }
